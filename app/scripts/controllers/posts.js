33ab1fb1717c60cc7288d510b9b6c960ac52013e
Bernardo Botelho <botelho.bernardo@gmail.com>
2015-10-19 20:54:50 +0000

    Comments, user profile, Firebase deploy

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,11 +1,15 @@
 'use strict';
 
-app.controller('PostsCtrl', function ($scope, $location, Post) {
+app.controller('PostsCtrl', function ($scope, $location, Post, Auth) {
 	$scope.posts = Post.all;
 
 	$scope.post = {url: 'http://', title: ''};
 
 	$scope.deletePost = function(post) {
 		Post.delete(post);
+	};
+
+	$scope.canDeletePost = function(post) {
+		return post.creatorUID === Auth.user.uid; 
 	};
 });
--- app/scripts/controllers/postview.js
+++ app/scripts/controllers/postview.js
@@ -1,5 +1,33 @@
 'use strict';
 
-app.controller('PostViewCtrl', function($scope, $routeParams, Post) {
+app.controller('PostViewCtrl', function($scope, $routeParams, Post, Auth) {
 	$scope.post = Post.get($routeParams.postId);
+	$scope.comments = Post.comments($routeParams.postId);
+
+	$scope.user = Auth.user;
+	$scope.signedIn = Auth.signedIn;
+
+	$scope.canDeleteComment = function(comment) {
+		return comment.creatorUID === $scope.user.uid;
+	};
+
+	$scope.deleteComment = function (comment) {
+		$scope.comments.$remove(comment);
+	};
+
+	$scope.addComment = function() {
+		if (!$scope.commentText || $scope.commentText === '') {
+			return;
+		}
+	
+		var comment = {
+			text: $scope.commentText,
+			creator: $scope.user.profile.username,
+			creatorUID: $scope.user.uid
+		};
+
+		$scope.comments.$add(comment);
+
+		$scope.commentText = '';
+	};
 });
